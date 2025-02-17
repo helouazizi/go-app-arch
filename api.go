1fba440e4cadbaff7a55c17a5ef630baee5b917c
helouazizi <ouazizi2code@gmail.com>
2025-02-17 19:47:34 +0100

    add api

--- api.go
+++ api.go
@@ -15,11 +15,14 @@ func NewApiServer(add string) *ApiServer {
 
 func (s *ApiServer) Run() error {
 	router := http.NewServeMux()
-
+	router.HandleFunc("/users/{userId}", func(w http.ResponseWriter, r *http.Request) {
+		userId := r.PathValue("userId")
+		w.Write([]byte("hi " + userId+"\n"))
+	})
 	server := http.Server{
 		Addr:    s.Add,
 		Handler: router,
 	}
-	log.Printf("server is running on post %s >>> http://localhost:%s", s.Add, s.Add)
+	log.Printf("server is running on post %s >>> http://localhost%s", s.Add, s.Add)
 	return server.ListenAndServe()
 }
