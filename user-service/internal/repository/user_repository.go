b0a3394bfac87722de100ae73072cc53f1505ef2
helouazizi <ouazizi2code@gmail.com>
2025-02-18 17:41:55 +0100

    add map storin

--- user-service/internal/repository/user_repository.go
+++ user-service/internal/repository/user_repository.go
@@ -6,25 +6,35 @@ import (
 )
 
 type InMemoryUserRepository struct {
-	users []*model.User
+	//Mutx   sync.Mutex
+	users map[int]*model.User
+	//NextId int
 }
+
+func NewInMemoryUserRepo() *InMemoryUserRepository {
+	return &InMemoryUserRepository{
+		make(map[int]*model.User),
+	}
+}
+
 type UserRepository interface {
 	CreateUser(name, email string) (*model.User, error)
-	GetUserByID(id int64) (*model.User, error)
-	ListUsers() ([]*model.User, error)
+	GetUserByID(id int) (*model.User, error)
+	UpdateUser(id int, name string) (*model.User, error)
+	ListUsers() (*map[int]*model.User, error)
 }
 
 func (repo *InMemoryUserRepository) CreateUser(name, email string) (*model.User, error) {
 	user := &model.User{
-		ID:    int64(len(repo.users) + 1),
+		ID:    int(len(repo.users) + 1),
 		Name:  name,
 		Email: email,
 	}
-	repo.users = append(repo.users, user)
+	repo.users[int(user.ID)] = user
 	return user, nil
 }
 
-func (repo *InMemoryUserRepository) GetUserByID(id int64) (*model.User, error) {
+func (repo *InMemoryUserRepository) GetUserByID(id int) (*model.User, error) {
 	/*for _, user := range repo.users {
 		if user.ID == id {
 			return user, nil
@@ -37,6 +47,18 @@ func (repo *InMemoryUserRepository) GetUserByID(id int64) (*model.User, error) {
 	return user, nil
 }
 
-func (repo *InMemoryUserRepository) ListUsers() ([]*model.User, error) {
-	return repo.users, nil
+func (repo *InMemoryUserRepository) UpdateUser(id int, name string) (*model.User, error) {
+	/*var user *model.User
+	for _, usr := range repo.users {
+		if usr.ID == id {
+			user = usr
+		}
+	}*/
+	user := repo.users[id]
+	user.Name = name
+	return user, nil
+}
+
+func (repo *InMemoryUserRepository) ListUsers() (*map[int]*model.User, error) {
+	return &repo.users, nil
 }
